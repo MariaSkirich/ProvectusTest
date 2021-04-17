7fd91206e35280c2e06fdb2902aca764b91cc71f
MariaSkirich <mskirich@gmail.com>
2021-04-17 23:06:09 +0300

    fixed some steps in test

--- src/test/java/com/provectus/pages/FormsPage.java
+++ src/test/java/com/provectus/pages/FormsPage.java
@@ -1,6 +1,7 @@
 package com.provectus.pages;
 
 import com.provectus.utils.CommonMethods;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
@@ -17,73 +18,69 @@ public class FormsPage extends CommonMethods {
     @FindBy(xpath = "//input[@id='firstName']")
     public WebElement firstNameBox;
 
-    @FindBy (xpath = "//input[@id='lastName']")
+    @FindBy(xpath = "//input[@id='lastName']")
     public WebElement lastNameBox;
 
-    @FindBy (xpath = "//input[@id='userEmail']")
+    @FindBy(xpath = "//input[@id='userEmail']")
     public WebElement userEmailBox;
 
-    @FindBy (xpath = "//input[@id='gender-radio-1']")
+    @FindBy(xpath = "//input[@id='gender-radio-1']")
     public WebElement maleGender;
 
-    @FindBy (xpath = "//input[@id='gender-radio-2']")
+    @FindBy(xpath = "//input[@id='gender-radio-2']")
     public WebElement femaleGender;
 
-    @FindBy (xpath = "//input[@id='gender-radio-2']")
+    @FindBy(xpath = "//input[@id='gender-radio-2']")
     public WebElement otherGender;
 
-    @FindBy (xpath = "//input[@id='userNumber']")
+    @FindBy(xpath = "//input[@id='userNumber']")
     public WebElement phoneNumber;
 
-    @FindBy (xpath = "//input[@id='dateOfBirthInput']")
+    @FindBy(xpath = "//input[@id='dateOfBirthInput']")
     public WebElement dateOfBirthBox;
 
-    @FindBy (xpath = "//select[@class='react-datepicker__month-select']")
+    @FindBy(xpath = "//select[@class='react-datepicker__month-select']")
     public WebElement monthDD;
 
-    @FindBy (xpath = "//select[@class='react-datepicker__year-select']")
+    @FindBy(xpath = "//select[@class='react-datepicker__year-select']")
     public WebElement yearDD;
 
-    @FindBy (xpath = "//div[@class='react-datepicker__week']")
+    @FindBy(xpath = "//div[@class='react-datepicker__week']")
     public List<WebElement> weeksAndDays;
 
-    @FindBy (xpath = "//input[@id='hobbies-checkbox-1']")
+    @FindBy(xpath = "//input[@id='hobbies-checkbox-1']")
     public WebElement sportsHobby;
 
-    @FindBy (xpath = "//input[@id='hobbies-checkbox-2']")
+    @FindBy(xpath = "//input[@id='hobbies-checkbox-2']")
     public WebElement readingHobby;
 
-    @FindBy (xpath = "//input[@id='hobbies-checkbox-3']")
+    @FindBy(xpath = "//input[@id='hobbies-checkbox-3']")
     public WebElement musicHobby;
 
-    @FindBy (xpath = "//input[@id='uploadPicture']")
+    @FindBy(xpath = "//input[@id='uploadPicture']")
     public WebElement uploadPicture;
 
-    @FindBy (xpath = "//div[@class='subjects-auto-complete__input']")
+    @FindBy(xpath = "//input[@id='subjectsInput']")
     public WebElement subjectsAutoComplete;
 
-    @FindBy (xpath = "//textarea[@class='form-control']")
+    @FindBy(xpath = "//textarea[@class='form-control']")
     public WebElement textArea;
 
-    @FindBy (xpath = "//div[@id='state']")
+    @FindBy(xpath = "//input[@id='react-select-3-input']")
     public WebElement state;
 
-    @FindBy (xpath = "//div[@id='city']")
+    @FindBy(xpath = "//input[@id='react-select-4-input']")
     public WebElement city;
 
-    @FindBy (xpath = "//button[@id='submit']")
+    @FindBy(xpath = "//button[@id='submit']")
     public WebElement submitButton;
 
-    @FindBy (xpath = "//div[@id='example-modal-sizes-title-lg']")
+    @FindBy(xpath = "//div[@id='example-modal-sizes-title-lg']")
     public WebElement message;
 
-    public FormsPage(){
+    public FormsPage() {
         PageFactory.initElements(driver, this);
     }
-
-
-
-
 
 
 }
--- src/test/java/com/provectus/testcases/fillFormTest.java
+++ src/test/java/com/provectus/testcases/fillFormTest.java
@@ -5,6 +5,7 @@ import com.provectus.utils.CommonMethods;
 import com.provectus.utils.ConfigsReader;
 import com.provectus.utils.Constants;
 import org.openqa.selenium.By;
+import org.openqa.selenium.InvalidElementStateException;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
@@ -14,7 +15,7 @@ public class fillFormTest extends CommonMethods {
 
     @Test(groups = "smoke")
     public void fillForm() throws InterruptedException {
-        FormsPage formsPage=new FormsPage();
+        FormsPage formsPage = new FormsPage();
         locatingElement(formsPage.forms);
         clickingOnSVG(formsPage.forms);
         click(formsPage.practiceForm);
@@ -22,10 +23,10 @@ public class fillFormTest extends CommonMethods {
         sendText(formsPage.lastNameBox, studentInfo("lastName"));
         sendText(formsPage.userEmailBox, studentInfo("email"));
 
-        String gender=studentInfo("gender");
-        if(gender.equalsIgnoreCase("Female")){
+        String gender = studentInfo("gender");
+        if (gender.equalsIgnoreCase("Female")) {
             jsClick(formsPage.femaleGender);
-        } else if (gender.equalsIgnoreCase("Male")){
+        } else if (gender.equalsIgnoreCase("Male")) {
             jsClick(formsPage.maleGender);
         } else {
             jsClick(formsPage.otherGender);
@@ -33,36 +34,35 @@ public class fillFormTest extends CommonMethods {
 
         sendText(formsPage.phoneNumber, studentInfo("mobile"));
 
-        String dateOfBirth=studentInfo("dateOfBirth");
-        String[] DOB=dateOfBirth.split("/");
-        String month=DOB[0];
-        String day=DOB[1];
-        String year=DOB[2];
+        String dateOfBirth = studentInfo("dateOfBirth");
+        String[] DOB = dateOfBirth.split("/");
+        String month = DOB[0];
+        String day = DOB[1];
+        String year = DOB[2];
 
         //click(formsPage.dateOfBirthBox);
 
         //calendarHandling(formsPage.monthDD, formsPage.yearDD, month, year, day, formsPage.weeksAndDays);
 
 
-        String subjects=studentInfo("subjects");
-        String[] sub=subjects.split(", ");
-        //THIS ANNOYING AUTOCOMPLETE FIELD DOESN'T WORK
-        sendText(formsPage.subjectsAutoComplete, sub[0]);
-        formsPage.subjectsAutoComplete.sendKeys(Keys.ENTER);
-        Thread.sleep(2000);
-        for(int i=0; i<sub.length; i++){
+        String subjects = studentInfo("subjects");
+        String[] sub = subjects.split(", ");
+
+        for (int i = 0; i < sub.length; i++) {
             sendText(formsPage.subjectsAutoComplete, sub[i]);
+            getWait();
             formsPage.subjectsAutoComplete.sendKeys(Keys.ENTER);
         }
+        Thread.sleep(2000);
 
         String hobby = studentInfo("hobbies").toLowerCase();
-        if(hobby.contains("music")){
+        if (hobby.contains("music")) {
             locatingElement(formsPage.musicHobby);
             jsClick(formsPage.musicHobby);
-        } else if (hobby.contains("reading")){
+        } else if (hobby.contains("reading")) {
             locatingElement(formsPage.musicHobby);
             jsClick(formsPage.readingHobby);
-        } else if (hobby.contains("sports")){
+        } else if (hobby.contains("sports")) {
             locatingElement(formsPage.sportsHobby);
             jsClick(formsPage.sportsHobby);
         }
@@ -73,9 +73,11 @@ public class fillFormTest extends CommonMethods {
 
         //the same problem with autocomplete
         sendText(formsPage.state, studentInfo("state"));
-        sendText(formsPage.city, "city");
+        formsPage.state.sendKeys(Keys.ENTER);
+        sendText(formsPage.city, studentInfo("city"));
+        formsPage.city.sendKeys(Keys.ENTER);
 
-        click(formsPage.submitButton);
+        jsClick(formsPage.submitButton);
 
         formsPage.message.isDisplayed();
 
